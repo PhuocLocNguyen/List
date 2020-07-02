8aae452b4f8256cfebdb55ff60d1bf5b5482b63e
PhuocLoc <nguyenphuocloc095@gmail.com>
2020-07-02 11:48:42 +0700

    Add Funciton to Reducer

--- App.js
+++ App.js
@@ -4,54 +4,6 @@ import List from './src/components/List';
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
-// const defaultState = {
-//   words: [
-//     {id: 1, en: 'One', vn: 'Mot', isMemorized: true},
-//     {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
-//     {id: 3, en: 'Three', vn: 'Ba', isMemorized: false},
-//     {id: 4, en: 'Four', vn: 'Bon', isMemorized: false},
-//     {id: 5, en: 'Five', vn: 'Nam', isMemorized: true},
-//   ],
-//   shouldShowForm: false,
-//   filterMode: 'Show_All',
-// };
-// const store = createStore((state = defaultState, action) => {
-//   switch (action.type) {
-//     case 'TOGGLE_FORM':
-//       return {...state, shouldShowForm: !state.shouldShowForm};
-
-//     case 'ADD_WORD':
-//       const newWordsAdd = [...state.words];
-//       const newWordAdd = {
-//         id: state.words.length + 1,
-//         en: action.en,
-//         vn: action.vn,
-//         isMemorized: false,
-//       };
-//       newWordsAdd.unshift(newWordAdd);
-//       return {...state, words: newWordsAdd, shouldShowForm: false};
-
-//     case 'TOGGLE_MEMORIZED':
-//       const newWordsToggleMemorized = state.words.map(word => {
-//         if (word.id === action.id) {
-//           const newWordToggleMemorized = {
-//             ...word,
-//             isMemorized: !word.isMemorized,
-//           };
-//           return newWordToggleMemorized;
-//         }
-//         return word;
-//       });
-//       return {...state, words: newWordsToggleMemorized};
-
-//     case 'REMOVE':
-//       const newWordsRemove = state.words.filter(word => action.id !== word.id);
-//       return {...state, words: newWordsRemove};
-
-//     default:
-//       return state;
-//   }
-// });
 const defaultWords = [
   {id: 1, en: 'One', vn: 'Mot', isMemorized: true},
   {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
@@ -61,10 +13,44 @@ const defaultWords = [
 ];
 
 function wordReducer(state = defaultWords, action) {
-  return state;
+  switch (action.type) {
+    case 'ADD_WORD':
+      const newWordsAdd = [...state];
+      const newWordAdd = {
+        id: state.length + 1,
+        en: action.en,
+        vn: action.vn,
+        isMemorized: false,
+      };
+      newWordsAdd.unshift(newWordAdd);
+      return newWordsAdd;
+
+    case 'TOGGLE_MEMORIZED':
+      const newWordsToggleMemorized = state.map(word => {
+        if (word.id === action.id) {
+          const newWordToggleMemorized = {
+            ...word,
+            isMemorized: !word.isMemorized,
+          };
+          return newWordToggleMemorized;
+        }
+        return word;
+      });
+      return newWordsToggleMemorized;
+
+    case 'REMOVE':
+      const newWordsRemove = state.filter(word => action.id !== word.id);
+      return newWordsRemove;
+
+    default:
+      return state;
+  }
 }
 
 function shouldShowFormReducer(state = false, action) {
+  if (action.type === 'TOGGLE_FORM' || action.type === 'ADD_WORD') {
+    return !state;
+  }
   return state;
 }
 
