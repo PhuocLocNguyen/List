e85a120ed29e0624fc274a552b4135a3e2ec7b51
PhuocLoc <nguyenphuocloc095@gmail.com>
2020-07-02 11:17:41 +0700

    reducer

--- App.js
+++ App.js
@@ -1,56 +1,78 @@
 import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
 import List from './src/components/List';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
-const defaultState = {
-  words: [
-    {id: 1, en: 'One', vn: 'Mot', isMemorized: true},
-    {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
-    {id: 3, en: 'Three', vn: 'Ba', isMemorized: false},
-    {id: 4, en: 'Four', vn: 'Bon', isMemorized: false},
-    {id: 5, en: 'Five', vn: 'Nam', isMemorized: true},
-  ],
-  shouldShowForm: false,
-  filterMode: 'Show_All',
-};
-const store = createStore((state = defaultState, action) => {
-  switch (action.type) {
-    case 'TOGGLE_FORM':
-      return {...state, shouldShowForm: !state.shouldShowForm};
 
-    case 'ADD_WORD':
-      const newWordsAdd = [...state.words];
-      const newWordAdd = {
-        id: state.words.length + 1,
-        en: action.en,
-        vn: action.vn,
-        isMemorized: false,
-      };
-      newWordsAdd.unshift(newWordAdd);
-      return {...state, words: newWordsAdd, shouldShowForm: false};
+// const defaultState = {
+//   words: [
+//     {id: 1, en: 'One', vn: 'Mot', isMemorized: true},
+//     {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
+//     {id: 3, en: 'Three', vn: 'Ba', isMemorized: false},
+//     {id: 4, en: 'Four', vn: 'Bon', isMemorized: false},
+//     {id: 5, en: 'Five', vn: 'Nam', isMemorized: true},
+//   ],
+//   shouldShowForm: false,
+//   filterMode: 'Show_All',
+// };
+// const store = createStore((state = defaultState, action) => {
+//   switch (action.type) {
+//     case 'TOGGLE_FORM':
+//       return {...state, shouldShowForm: !state.shouldShowForm};
 
-    case 'TOGGLE_MEMORIZED':
-      const newWordsToggleMemorized = state.words.map(word => {
-        if (word.id === action.id) {
-          const newWordToggleMemorized = {
-            ...word,
-            isMemorized: !word.isMemorized,
-          };
-          return newWordToggleMemorized;
-        }
-        return word;
-      });
-      return {...state, words: newWordsToggleMemorized};
+//     case 'ADD_WORD':
+//       const newWordsAdd = [...state.words];
+//       const newWordAdd = {
+//         id: state.words.length + 1,
+//         en: action.en,
+//         vn: action.vn,
+//         isMemorized: false,
+//       };
+//       newWordsAdd.unshift(newWordAdd);
+//       return {...state, words: newWordsAdd, shouldShowForm: false};
 
-    case 'REMOVE':
-      const newWordsRemove = state.words.filter(word => action.id !== word.id);
-      return {...state, words: newWordsRemove};
+//     case 'TOGGLE_MEMORIZED':
+//       const newWordsToggleMemorized = state.words.map(word => {
+//         if (word.id === action.id) {
+//           const newWordToggleMemorized = {
+//             ...word,
+//             isMemorized: !word.isMemorized,
+//           };
+//           return newWordToggleMemorized;
+//         }
+//         return word;
+//       });
+//       return {...state, words: newWordsToggleMemorized};
 
-    default:
-      return state;
-  }
+//     case 'REMOVE':
+//       const newWordsRemove = state.words.filter(word => action.id !== word.id);
+//       return {...state, words: newWordsRemove};
+
+//     default:
+//       return state;
+//   }
+// });
+const defaultWords = [
+  {id: 1, en: 'One', vn: 'Mot', isMemorized: true},
+  {id: 2, en: 'Two', vn: 'Hai', isMemorized: false},
+  {id: 3, en: 'Three', vn: 'Ba', isMemorized: false},
+  {id: 4, en: 'Four', vn: 'Bon', isMemorized: false},
+  {id: 5, en: 'Five', vn: 'Nam', isMemorized: true},
+];
+
+function wordReducer(state = defaultWords, action) {
+  return state;
+}
+
+function shouldShowFormReducer(state = false, action) {
+  return state;
+}
+
+const reducer = combineReducers({
+  words: wordReducer,
+  shouldShowForm: shouldShowFormReducer,
 });
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
